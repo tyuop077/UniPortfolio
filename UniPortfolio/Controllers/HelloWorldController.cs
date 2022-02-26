8eed9abc0c7cc7e02146e30764126df15184a266
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-02-26 10:35:57 +0300

    Initial

---
+++ UniPortfolio/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace UniPortfolio.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // HelloWorld/Welcome
+        /*public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }*/
+        /*public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ UniPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
